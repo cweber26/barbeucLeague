76f418e91cf55b8a35ab62d9f1144533fca5c962
Cédric Weber <cedric.weber@decathlon.com>
2020-01-02 16:08:43 +0100

    correction message auto cancel

--- back/control.js
+++ back/control.js
@@ -3,8 +3,6 @@ function controlAndCancelOrRelaunch() {
         if(isTheMatchInProgress()){
             if ((numberPlayerInMatch > 0 && numberPlayerInMatch < minPlayerForAutoCancelation)) {
                 cancelMatchAndSendMail(true);
-                sendMailSimple("Pense à annuler la réservation du match " + matchDayGapInFrench(true), "<h2>Pense à annuler la réservation " + nextMatchStadiumName + "</h2>");
-
             } else if (numberPlayerInMatch >= minPlayerForAutoCancelation && numberPlayerInMatch < numberPlayerMatch) {
                 relaunchInscriptionMailForPlayersWithoutAnswer();
             }
